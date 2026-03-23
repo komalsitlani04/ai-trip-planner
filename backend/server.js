31461a90952a31e2a6ed6cfbc2a4ef1beff56a81
Komal Sitlani <sitlanikomal05@gmail.com>
2026-03-23 10:06:12 -0400

    Initial backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+connectDB();
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Test Route
-app.get("/", (req, res) => {
-  res.send("AI Trip Planner Server Running 🚀");
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Backend is working 🚀" });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
