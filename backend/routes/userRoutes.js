d8b90e0c4bcbc1206458920f17fe09767864406c
Komal Sitlani <sitlanikomal05@gmail.com>
2026-03-25 09:34:09 -0400

    Implemented user profile API with secure data fetching

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const protect = require("../middleware/authMiddleware");
+const User = require("../models/User");
 
 const router = express.Router();
 
-// Protected route
-router.get("/profile", protect, (req, res) => {
-  res.json({
-    message: "Access granted to protected route 🔐",
-    userId: req.user,
-  });
+// Get user profile
+router.get("/profile", protect, async (req, res) => {
+  try {
+    const user = await User.findById(req.user).select("-password");
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 module.exports = router;
